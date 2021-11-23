fb609c425cfca8b51aa838479a1a58b9e7eeb795
Grazia Stracquadanio <graziastracquadanio@Grazias-MBP.Home>
2021-11-23 12:48:21 +0000

    chore: added typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,5 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "gracegrace",
   },
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-typescript"],
 };
